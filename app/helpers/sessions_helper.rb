367478bb32c45e65ba8d5030c04f6459822c9bdc
Roger <marion.velard@gmail.com>
2018-08-05 18:45:46 +0200

    remember box

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,8 +7,23 @@ module SessionsHelper
         #Methode sera utilisé à plusieurs endroit. Appelé grace au helper dans différents controller.
     end
 
+     # Remembers a user in a persistent session.
+    def remember(user)
+         user.remember
+         cookies.permanent.signed[:user_id] = user.id
+         cookies.permanent[:remember_token] = user.remember_token
+    end
+
     def current_user
-        @current_user ||= User.find_by(id: session[:user_id])
+        if (user_id = session[:user_id])
+            @current_user ||= User.find_by(id: user_id)
+        elsif (user_id = cookies.signed[:user_id])
+            user = User.find_by(id: user_id)
+            if user && user.authenticated?(cookies[:remember_token])
+              log_in user
+              @current_user = user
+            end
+          end
         #Précupére l'ID du user actuel (current_user) sur les pages suivantes. Seul le current_user pourra acceder à 
         #ses actions (update, delete) et son profil
     end
@@ -19,10 +34,23 @@ module SessionsHelper
       #c'est-à-dire si current_user n'est pas nul.
     end
 
+    # Forgets a persistent session.
+    def forget(user)
+      user.forget
+      cookies.delete(:user_id)
+      cookies.delete(:remember_token)
+    end
+
     def log_out
+        forget(current_user)
         session.delete(:user_id)
         #Deconnecter le current_user
         @current_user = nil
-      end
+    end
+
+    def destroy
+        log_out if logged_in?
+        redirect_to root_url
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_04_192705) do
+ActiveRecord::Schema.define(version: 2018_08_05_141525) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_08_04_192705) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
