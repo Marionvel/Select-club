a5754b96de276c8f245db19e80b70e4882180967
Roger <marion.velard@gmail.com>
2018-08-06 19:42:41 +0200

    Test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -4,4 +4,11 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+#La route du Login correspond à l'action
+  test "should get new" do
+    get login_path
+    assert_response :success
+  end
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,4 +4,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  #Se loger avec des infos invalide 
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
 end
